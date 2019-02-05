724ad12aa3b8f514a92e19a4fcc1de4044174412
M.F.Aziz <azizsagi@yahoo.com>
2019-02-05 23:52:58 +0300

    Auth0 Working with API's Extended for Rules per Application feature
    Auth0 Working with API's Extended for Rules per Application feature

---
+++ auth0-variables.js
@@ -0,0 +1,5 @@
+var AUTH0_CLIENT_ID = 'DMIVDQhGASysPTXKfbLSHCs4rSJ7gW2s';
+var AUTH0_DOMAIN = 'mandhar.auth0.com';
+var AUTH0_AUDIENCE = 'https://mandhar.auth0.com/api/v2/';
+var AUTH0_CALLBACK_URL = location.href;
+
